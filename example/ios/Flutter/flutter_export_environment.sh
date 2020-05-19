c4a4df0ebd6b1244574ca697ed01bdea56a2eb89
Alan <xujiaoweialan@gmail.com>
2020-05-19 16:52:10 +0800

    Initial commit

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/Alan/Document/Enviroment/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Alan/Document/PanLetter/Android/PropertyCheckFlutter/des_plugin/des_plugin/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Alan/Document/Enviroment/flutter/bin/cache/artifacts/engine/ios"
