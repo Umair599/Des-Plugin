d8e7e3a3350b63a0f3ed0dc61561c7031f603f36
Umair Khalid <umair.khalid.599@gmail.com>
2024-06-24 01:24:21 +0500

    upgraded environment depenecy on example pubspec

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/Alan/Document/Enviroment/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/Alan/Document/PanLetter/Android/PropertyCheckFlutter/des_plugin/des_plugin/example"
+export "FLUTTER_ROOT=/Users/umairkhalid/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/umairkhalid/Des-Plugin/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/Alan/Document/Enviroment/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
