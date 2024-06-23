fa75ed735f94cf807b59b403a65460e685a2b0d7
Umair Khalid <umair.khalid.599@gmail.com>
2024-06-24 01:11:04 +0500

    update android dependencies

--- android/src/main/java/com/pensoon/des_plugin/DesPlugin.java
+++ android/src/main/java/com/pensoon/des_plugin/DesPlugin.java
@@ -1,32 +1,40 @@
 package com.pensoon.des_plugin;
 
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /**
  * DesPlugin
  */
-public class DesPlugin implements MethodCallHandler {
-    /**
-     * Plugin registration.
-     */
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "des_plugin");
-        channel.setMethodCallHandler(new DesPlugin());
+public class DesPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
+    private MethodChannel channel;
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "des_plugin");
+        channel.setMethodCallHandler(this);
     }
 
     @Override
-    public void onMethodCall(MethodCall call, Result result) {
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
         switch (call.method) {
             case "encrypt":
                 try {
                     result.success(DesCryptUtils.encrypt(call.argument("key").toString(), call.argument("data").toString()));
                 } catch (Exception e) {
                     e.printStackTrace();
-                    result.error(e.getMessage(),"",null);
+                    result.error(e.getMessage(), "", null);
                 }
                 break;
             case "decrypt":
@@ -34,24 +42,43 @@ public class DesPlugin implements MethodCallHandler {
                     result.success(DesCryptUtils.decrypt(call.argument("key").toString(), call.argument("data").toString()));
                 } catch (Exception e) {
                     e.printStackTrace();
-                    result.error(e.getMessage(),"",null);
+                    result.error(e.getMessage(), "", null);
                 }
                 break;
             case "threeEncrypt":
+                // Implement if needed
                 break;
             case "threeDecrypt":
                 try {
-                    result.success(DesCryptUtils.decode(call.argument("key").toString(), call.argument("data").toString() ,
-                            call.argument("iv").toString()));
+                    result.success(DesCryptUtils.decode(call.argument("key").toString(), call.argument("data").toString(), call.argument("iv").toString()));
                 } catch (Exception e) {
                     e.printStackTrace();
-                    result.error(e.getMessage(),"",null);
+                    result.error(e.getMessage(), "", null);
                 }
                 break;
             default:
                 result.notImplemented();
                 break;
         }
+    }
 
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        // Handle activity attachments if needed
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        // Handle activity detachment for config changes if needed
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        // Handle activity re-attachments for config changes if needed
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        // Handle activity detachment if needed
     }
 }
